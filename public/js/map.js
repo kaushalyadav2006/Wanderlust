d9c7b8014eed12316bae318ff093c77efa90472d
kaushalYadav2006 <yadavkaushal2023@gmail.com>
2026-04-21 18:59:33 +0530

    feat: update map initialization and coordinate handling in show page

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,13 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  projection: "globe",
+  zoom: 8, // increase zoom for city view
+  center: coordinates, // [ longitude , latitude ] // New Delhi
+});
 
-  mapboxgl.accessToken = mapToken;  
-  const map = new mapboxgl.Map({ 
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    projection: 'globe',
-    zoom: 9, // increase zoom for city view
-    center: [77.2090, 28.6139] // [ longitude , latitude ] // New Delhi
-  });
+// Create a default Marker and add it to the map.
+const marker1 = new mapboxgl.Marker({color:"red"})
+  .setLngLat(coordinates) //listing.geometry.coordinates
+  .addTo(map);
