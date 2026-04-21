e1ed56e62a9cea262812a6181be6ff21a72f2b4c
kaushalYadav2006 <yadavkaushal2023@gmail.com>
2026-04-21 19:34:35 +0530

    project phase-3 (part-b) feat: update map handling by using listing geometry directly in show page

--- public/js/map.js
+++ public/js/map.js
@@ -2,12 +2,17 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v12",
+  // style: "mapbox://styles/mapbox/standard",
+  // style: "mapbox://styles/mapbox/satellite-streets-v12",
   projection: "globe",
   zoom: 8, // increase zoom for city view
-  center: coordinates, // [ longitude , latitude ] // New Delhi
+  center: listing.geometry.coordinates, // [ longitude , latitude ] // New Delhi
 });
 
 // Create a default Marker and add it to the map.
-const marker1 = new mapboxgl.Marker({color:"red"})
-  .setLngLat(coordinates) //listing.geometry.coordinates
+const marker1 = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates) //listing.geometry.coordinates
+  .setPopup(new mapboxgl.Popup({ offset: 30 })
+  .setHTML(`<h4>${listing.location}</h4><p>Exact location will be provided after booking</p>`))
   .addTo(map);
+
