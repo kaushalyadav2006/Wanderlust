297dae63df4da3abd35a2c7c8e15ae30c77cb453
kaushalYadav2006 <yadavkaushal2023@gmail.com>
2026-04-28 20:06:13 +0530

    using this for render all the listings on the live links

---
+++ init/index2Render.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config({ quiet: true });
+}
+
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+const User = require("../models/user.js");
+
+const Mongo_URL = process.env.ATLASDB_URL;
+
+main()
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(Mongo_URL);
+  await initDB();
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+
+  const user = await User.findOne(); // fetch real user
+
+  const data = initData.data.map((obj) => ({
+    ...obj,
+    owner: user._id,
+  }));
+
+  await Listing.insertMany(data);
+  console.log("Data initialized");
+};
